449925ce0dcf707764e1510d71e9f43f3610043c
Matheus Paschoal <paschoal@gmail.com>
2020-06-12 09:44:24 -0400

    qutebrowser monoid font

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,29 +1,26 @@
-c.fonts.monospace = "Monoid"
-c.fonts.completion.entry = "16px monospace"
-c.fonts.completion.category = "16px monospace"
-c.fonts.debug_console = "16px monospace"
-c.fonts.downloads = "16px monospace"
-c.fonts.hints = "16px monospace"
-c.fonts.keyhint = "16px monospace"
-c.fonts.messages.error = "16px monospace"
-c.fonts.messages.info = "16px monospace"
-c.fonts.prompts = "16px monospace"
-c.fonts.statusbar = "16px monospace"
-c.fonts.tabs = "16px monospace"
+c.fonts.completion.entry = "16px Monoid"
+c.fonts.completion.category = "16px Monoid"
+c.fonts.debug_console = "16px Monoid"
+c.fonts.downloads = "16px Monoid"
+c.fonts.hints = "16px Monoid"
+c.fonts.keyhint = "16px Monoid"
+c.fonts.messages.error = "16px Monoid"
+c.fonts.messages.info = "16px Monoid"
+c.fonts.prompts = "16px Monoid"
+c.fonts.statusbar = "16px Monoid"
+c.fonts.tabs = "16px Monoid"
 
-# c.fonts.web.family.standard = 'monospace'
-# c.fonts.web.family.fixed = 'monospace'
-# c.fonts.web.family.serif = 'monospace'
-# c.fonts.web.family.sans_serif = 'monospace'
-# c.fonts.web.family.cursive = 'monospace'
-# c.fonts.web.family.fantasy = 'monospace'
+# c.fonts.web.family.standard = 'Monoid'
+# c.fonts.web.family.fixed = 'Monoid'
+# c.fonts.web.family.serif = 'Monoid'
+# c.fonts.web.family.sans_serif = 'Monoid'
+# c.fonts.web.family.cursive = 'Monoid'
+# c.fonts.web.family.fantasy = 'Monoid'
 
 c.content.geolocation = False
 c.content.media_capture = False
 c.content.notifications = True
-c.content.pdfjs = True
-
-c.content.headers.user_agent = 'Mozila/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0'
+c.content.pdfjs = False
 
 c.completion.height = '20%'
 
