1decb5b48f85ae618f5b9e65c380265bf78c2aef
Matheus Paschoal <paschoal@gmail.com>
2024-02-18 22:51:15 -0500

    tree sitter not indenting ruby file wrong because of .end

--- nvim/config/lua/tree-sitter.lua
+++ nvim/config/lua/tree-sitter.lua
@@ -1,4 +1,9 @@
 require('nvim-treesitter.configs').setup {
   highlight = { enable = true },
   indent = { enable = true },
+  matchup = { enable = true },
 }
+
+-- Ruby indentation is broken
+-- using tree-sitter due the .end statement.
+vim.cmd('autocmd FileType ruby setlocal indentkeys-=.')
