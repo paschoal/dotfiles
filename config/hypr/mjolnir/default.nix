100bf9f4e88bfab56e93f1e627ad11eea2441da0
Matheus Paschoal <paschoal@gmail.com>
2024-02-26 20:56:16 -0500

    mjolnir using swww instead of hyprpaper

--- config/hypr/mjolnir/default.nix
+++ config/hypr/mjolnir/default.nix
@@ -2,22 +2,13 @@
 
 {
   home.packages = [
-    pkgs.hyprpaper
+    pkgs.swww
   ];
 
   xdg.configFile = {
     hypr = {
       source = ./config;
       recursive = true;
-    };
-
-    "hypr/hyprpaper.conf" = {
-      text = ''
-        preload=$HOME/.wallpaper/ow.landscape.jpg
-        preload=$HOME/.wallpaper/ow.portrait.jpg
-        wallpaper=DP-3,$HOME/.wallpaper/ow.landscape.jpg
-        wallpaper=HDMI-A-1,$HOME/.wallpaper/ow.portrait.jpg
-      '';
     };
   };
 }
