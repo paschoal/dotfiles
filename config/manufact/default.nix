f329f1e18f28c5bea0ab52188723139472afefca
Matheus Paschoal <paschoal@gmail.com>
2024-04-05 23:13:00 -0400

    add cura; fix missing req for freecad

--- config/manufact/default.nix
+++ config/manufact/default.nix
@@ -1,7 +1,28 @@
 { config, pkgs, ... }:
 
-{
-  home.packages = [
-    freecad
-  ];
+with import <nixpkgs> {};
+let
+  file = appimageTools.wrapType2 rec {
+    name = "ultimaker-cura";
+    version = "5.7.0";
+    src = fetchurl {
+      url = "https://github.com/Ultimaker/Cura/releases/download/${version}/UltiMaker-Cura-${version}-linux-X64.AppImage";
+      hash = "sha256-5PaBhPJKqa8LxEHTRNTLqkcIfC2PkqmTWx9c1+dc7k0=";
+    };
+  };
+
+in {
+  config = {
+    home.packages = with pkgs; [
+      freecad
+    ];
+
+    home.file = {
+      "cura" = {
+        target = "./bin/cura";
+        enable = true;
+        source = "${file}/bin/ultimaker-cura";
+      };
+    };
+  };
 }
