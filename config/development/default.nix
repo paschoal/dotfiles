8a1e5c537052d5ec19359d8ce14d80f1c77d4275
Matheus Paschoal <paschoal@gmail.com>
2024-08-20 00:29:49 -0400

    mix and match of stable and unstable

--- config/development/default.nix
+++ config/development/default.nix
@@ -1,10 +1,12 @@
 { config, pkgs, ... }:
 
-{
-  home.packages = with pkgs; [
-    google-cloud-sdk
-    ruby_3_3
-    postgresql
-    dbeaver-bin
-  ];
-}
+let
+  stable-pkgs = import <nixos-stable> {};
+  in {
+    home.packages = [
+      pkgs.google-cloud-sdk
+      pkgs.ruby_3_3
+      pkgs.postgresql
+      stable-pkgs.dbeaver-bin
+    ];
+  }
