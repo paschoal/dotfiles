4a6c742431cebb58724088d571c10194ee7d6023
Matheus Paschoal <paschoal@gmail.com>
2024-10-26 02:34:52 -0400

    update warcraft-logs

--- games/wow/logs/default.nix
+++ games/wow/logs/default.nix
@@ -4,10 +4,10 @@ with import <nixpkgs> {};
 let
   file = appimageTools.wrapType2 rec {
     name = "wow-logs";
-    version = "v8.5.6";
+    version = "v8.14.4";
     src = fetchurl {
       url = "https://github.com/RPGLogs/Uploaders-warcraftlogs/releases/download/${version}/warcraftlogs-${version}.AppImage";
-      hash = "sha256-hT5UfBWSmVYwMQm2x+sxmjHyfxOCzbhAyu/XCVvABy8=";
+      hash = "sha256-vrzRw3c0IbDCjqo0FmUIBd9v79loJJ6GOYAv6xJQhvs=";
     };
   };
 
