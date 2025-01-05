8213d869cf1e90098b0fcfe101c0cc50acfcbbc0
Matheus Paschoal <paschoal@gmail.com>
2025-01-05 13:54:22 -0500

    mjolnir config

--- config/qutebrowser/mjolnir/config/config.py
+++ config/qutebrowser/mjolnir/config/config.py
@@ -1,10 +1,11 @@
 config.load_autoconfig()
 
-c.fonts.default_family ='Iosevka'
+c.fonts.default_family ='Monoid'
 c.fonts.default_size = '18pt'
 
 c.content.geolocation = False
 c.content.pdfjs = False
+c.content.dns_prefetch = True
 
 c.completion.height = '20%'
 c.completion.cmd_history_max_items = 2
@@ -16,3 +17,15 @@ c.downloads.location.directory = '~/downloads/'
 c.downloads.remove_finished = 3000
 
 c.zoom.default = '100%'
+
+c.url.searchengines = {
+        'DEFAULT': 'https://google.ca/search?hl=eng&q={}',
+        '!amazon': 'https://www.amazon.ca/s?k={}',
+        '!github': 'https://github.com/search?o=desc&s=stars&q={}',
+        '!gist': 'https://gist.github.com/search?q={}',
+        '!images': 'https://www.google.com/search?tbm=isch&q={}&tbs=imgo:1',
+        '!maps': 'https://www.google.com/maps/search/{}',
+        '!youtube': 'https://www.youtube.com/results?search_query={}',
+        '!reddit': 'https://www.reddit.com/search?q={}'
+}
+c.url.start_pages = 'https://google.ca/'
