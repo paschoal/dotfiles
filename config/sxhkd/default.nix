cca6eafd4aa7a86e618b1db2adca1c8d5f375493
Matheus Paschoal <paschoal@gmail.com>
2025-02-21 11:13:02 -0500

    single file, flag based options

--- config/sxhkd/default.nix
+++ config/sxhkd/default.nix
@@ -1,24 +1,55 @@
 { config, lib, pkgs, ... }:
 
 {
-  services.sxhkd = {
-    enable = true;
-    keybindings = {
-      "alt + d" = "rofi -show run";
-      "alt + s" = "rofi -show drun";
-      "alt + f" = "rofi-pass";
-      "alt + Tab" = "rofi -show window";
-      "alt + Return" = "alacritty";
+  options = {
+    sxhkd-config = {
+      rofi = lib.mkOption {
+        default = true;
+        type = lib.types.bool;
+        description = ''
+          Enable rofi keybindings
+        '';
+      };
 
-      "alt + {_,shift +}z" = "bspc node -{c,k}";
-      "alt + {_,shift +}{1-9,0}" = "bspc {desktop -f, node -d} '^{1-9,10}'";
-      "alt + {_,shift +}{h,j,k,l}" = "bspc node -{f,s} {west,south,north,east}";
-      "alt + {w,e,r,t}" = "bspc node -t {tiled,pseudo_tiled,floating,fullscreen}";
-      "alt + b" = "bspc desktop -l next";
+      bspwm = lib.mkOption {
+        default = true;
+        type = lib.types.bool;
+        description = ''
+          Enable bspwm keybindings
+        '';
+      };
     };
   };
 
-  systemd.user.services.sxhkd = {
+  config.services.sxhkd = {
+    enable = true;
+    keybindings = lib.mkMerge [
+      {
+        "alt + Return" = "alacritty";
+      }
+
+      (
+        lib.mkIf config.sxhkd-config.rofi {
+          "alt + d" = "rofi -show run";
+          "alt + s" = "rofi -show drun";
+          "alt + f" = "rofi-pass";
+          "alt + Tab" = "rofi -show window";
+        }
+      )
+
+      (
+        lib.mkIf config.sxhkd-config.bspwm {
+          "alt + {_,shift +}z" = "bspc node -{c,k}";
+          "alt + {_,shift +}{1-9,0}" = "bspc {desktop -f, node -d} '^{1-9,10}'";
+          "alt + {_,shift +}{h,j,k,l}" = "bspc node -{f,s} {west,south,north,east}";
+          "alt + {w,e,r,t}" = "bspc node -t {tiled,pseudo_tiled,floating,fullscreen}";
+          "alt + b" = "bspc desktop -l next";
+        }
+      )
+    ];
+  };
+
+  config.systemd.user.services.sxhkd = {
     Unit = {
       Description = "Simple X Hotkey Daemon";
       After = "graphical-session.target";
