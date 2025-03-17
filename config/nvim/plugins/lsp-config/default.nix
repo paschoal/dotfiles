da7a4719d7b3e7a54983f74c2d7fa7c68774c9b5
Matheus Paschoal <paschoal@gmail.com>
2025-03-17 13:25:44 -0400

    use containerized ruby-lsp

--- config/nvim/plugins/lsp-config/default.nix
+++ config/nvim/plugins/lsp-config/default.nix
@@ -9,9 +9,27 @@
         config = ''
           require("lspconfig").dartls.setup {}
           require("lspconfig").lua_ls.setup {}
-          -- require("lspconfig").ruby_lsp.setup {}
+          require("lspconfig").ruby_lsp.setup {
+            cmd = require("lspcontainers").command(
+              "ruby_lsp", {
+                image = "ruby-lsp",
+                cmd = function(runtime, image)
+                  return {
+                    "docker",
+                    "container",
+                    "run",
+                    "--interactive",
+                    "--rm",
+                    "--network=false",
+                    image
+                  }
+                end
+              }
+            ),
+          }
         '';
       }
+      lspcontainers-nvim
     ];
   };
 }
