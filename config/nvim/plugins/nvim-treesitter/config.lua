94df9bc24fb02fa5b4c787bcb17b1fca8ef9ff3f
Matheus Paschoal <paschoal@gmail.com>
2025-05-20 12:07:55 -0400

    update treesitter config

--- config/nvim/plugins/nvim-treesitter/config.lua
+++ config/nvim/plugins/nvim-treesitter/config.lua
@@ -10,3 +10,14 @@ require("nvim-treesitter.configs").setup {
 vim.wo.foldmethod = "expr"
 vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.wo.foldlevel = 99
+
+vim.filetype.add({
+  extension = {
+    gotmpl = "gotmpl",
+  },
+  pattern = {
+    [".*/templates/.*%.tpl"] = "helm",
+    [".*/templates/.*%.ya?ml"] = "helm",
+    ["helmfile.*%.ya?ml"] = "helm",
+  },
+})
