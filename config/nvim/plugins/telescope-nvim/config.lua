1ce30c3dedc200d7c42b1e2e4845ec580d53ed37
Matheus Paschoal <paschoal@gmail.com>
2025-06-12 12:18:02 -0400

    vimrc -> nvim lua config

--- config/nvim/plugins/telescope-nvim/config.lua
+++ config/nvim/plugins/telescope-nvim/config.lua
@@ -10,3 +10,6 @@ require("telescope").setup {
 }
 
 require("telescope").load_extension("fzf")
+
+vim.keymap.set("n", "<leader>ff", "<Cmd>Telescope find_files<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>fb", "<Cmd>Telescope buffers<CR>", { noremap = true, silent = true })
