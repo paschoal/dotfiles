77e411beba9a35772894fe79835dc9a7a37fd3c7
Matheus Paschoal <paschoal@gmail.com>
2025-06-27 11:29:30 -0400

    add steam configs

--- config/bspwm/default.nix
+++ config/bspwm/default.nix
@@ -26,10 +26,39 @@
           Launch Eww bar
         '';
       };
+
+      steam-games-fullscreen = lib.mkOption {
+        default = false;
+        type = lib.types.bool;
+        description = ''
+          Setup rule for all steam games to be kept in fullscreen
+        '';
+      };
+
+      steam-games-library = lib.mkOption {
+        default = ~/.steam;
+        type = lib.types.path;
+        description = ''
+          Steam library path
+        '';
+      };
     };
   };
 
   config.xdg.configFile = {
+    "bspwm/steam" = {
+      enable = config.bspwm-config.steam-games-fullscreen;
+      text = ''
+        #! /usr/bin/env nix-shell
+        #! nix-shell -i bash -p bash
+        ${lib.strings.toShellVar "library" config.bspwm-config.steam-games-library}
+        games=$(ls $library/steam/steamapps/appmanifest_*.acf | sed 's/[^0-9]*//g')
+        for game in $games; do
+          bspc rule -a steam_app_$game desktop=^5 state=fullscreen
+        done
+      '';
+    };
+
     "bspwm/bspwmrc" = {
       source = let
         base = ''
@@ -45,6 +74,7 @@
         '';
 
         bits = with lib.lists; [ base ]
+          ++ optional config.bspwm-config.steam-games-fullscreen "sh ./steam &"
           ++ optional config.bspwm-config.disable-caps "${pkgs.xorg.setxkbmap} -option 'caps:none' &"
           ++ optional config.bspwm-config.wallpaper "${pkgs.feh}/bin/feh --bg-fill ~/.wallpaper/landscape.jpg &"
           ++ optional config.bspwm-config.eww "${pkgs.eww}/bin/eww daemon && ${pkgs.eww}/bin/eww open bar &"
