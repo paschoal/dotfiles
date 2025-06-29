6133cd5d03b8ef6d0ed33fbcb0d73258932c83f2
Matheus Paschoal <paschoal@gmail.com>
2025-06-29 16:37:23 -0400

    add purge line macro

--- services/klipper/default.nix
+++ services/klipper/default.nix
@@ -140,6 +140,19 @@
       display_status = {};
       pause_resume = {};
 
+      "gcode_macro PURGE_LINE" = {
+        description = "Cura purge line";
+        gcode = "
+          G92 E0
+          G1 Z2.0 F3000
+          G1 X0.1 Y20 Z0.3 F5000.0
+          G1 X0.1 Y200 Z0.3 F1500 E15
+          G1 X0.4 Y200 Z0.3 F5000
+          G1 X0.4 Y20 Z0.3 F1500 E30
+          G92 E0
+        ";
+      };
+
       "gcode_macro CANCEL_PRINT" = {
         description = "Cancel the running print.";
         rename_existing = "CANCEL_PRINT_BASE";
