0419e7172267d46774162f0466173acd7df933da
Matheus Paschoal <paschoal@gmail.com>
2025-09-01 14:47:17 -0400

    switch ridill to use local dns

--- hosts/ridill/network.nix
+++ hosts/ridill/network.nix
@@ -1,8 +1,11 @@
 { config, lib, pkgs, ... }:
 {
   networking = {
+    nameservers = [ "127.0.0.1" "::1" ];
     hostName = "ridill";
     useDHCP = lib.mkDefault true;
     firewall.enable = false;
   };
+
+  services.resolved.enable = false;
 }
