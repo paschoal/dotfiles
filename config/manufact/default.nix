5a886a1ce1fdcb76dcd687f0faa4ee1080a1f017
Matheus Paschoal <paschoal@gmail.com>
2025-09-11 15:38:51 -0400

    streamline chromium and orca-slicer file

--- config/manufact/default.nix
+++ config/manufact/default.nix
@@ -1,13 +1,25 @@
 { config, pkgs, ... }:
 
-let
-  stable-pkgs = import <nixos-stable> { };
+let stable-pkgs = import <nixos-stable> { };
 in {
-  imports = [
-    ./orca-slicer
+  nixpkgs.config.permittedInsecurePackages = [
+    "libsoup-2.74.3"
   ];
 
   home.packages = [
     stable-pkgs.freecad
+    (
+      pkgs.appimageTools.wrapType2 rec {
+        pname = "orca-slicer";
+        version = "v2.3.0";
+        src = pkgs.fetchurl {
+          url = "https://github.com/SoftFever/OrcaSlicer/releases/download/${version}/OrcaSlicer_Linux_AppImage_${version}.AppImage";
+          hash = "sha256-cwediOw28GFdt5GdAKom/jAeNIum4FGGKnz8QEAVDAM=";
+        };
+        extraPkgs = pkgs: with pkgs; [
+          webkitgtk_4_0
+        ];
+      }
+    )
   ];
 }
