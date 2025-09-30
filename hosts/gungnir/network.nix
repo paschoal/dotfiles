f13884bf78a3c4dd514eb94eafc07160a91807dc
Matheus Paschoal <paschoal@gmail.com>
2025-09-30 07:04:03 -0400

    add more frequent wifi

--- hosts/gungnir/network.nix
+++ hosts/gungnir/network.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, ... }:
 
 {
   imports = [
@@ -23,5 +23,7 @@
     secretsFile = config.age.secrets.wifi-networks.path;
     networks."UairelessMov".pskRaw = "ext:UairelessMov";
     networks."Uaireless".pskRaw = "ext:Uaireless";
+    networks."Wireless".pskRaw = "ext:Wireless";
+    networks."Wireless-5G".pskRaw = "ext:Wireless";
   };
 }
