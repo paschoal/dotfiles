8ef546d7088fbbe7b24b6f9598b438a5b5802062
Matheus Paschoal <paschoal@gmail.com>
2025-11-28 22:22:53 -0500

    better conf

--- common/fonts/default.nix
+++ common/fonts/default.nix
@@ -1,10 +1,25 @@
 { pkgs, ... }:
 {
-  fonts.packages = with pkgs; [
-    iosevka
-    monoid
-    nerd-fonts.noto
-    nerd-fonts.iosevka
-    nerd-fonts.monoid
-  ];
+  fonts = {
+    packages = with pkgs; [
+      iosevka
+      monoid
+      noto-fonts
+      nerd-fonts.noto
+      nerd-fonts.iosevka
+      nerd-fonts.monoid
+    ];
+
+    enableDefaultFonts = true;
+    enableGhostscriptFonts = true;
+
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        serif = [ "Noto Serif" ];
+        sansSerif = [ "Noto Sans" ];
+        monospace = [ "Monoid" ];
+      };
+    };
+  };
 }
