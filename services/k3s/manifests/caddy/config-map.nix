280170955b991b3ceda5aad37dc847d02a05c691
Matheus Paschoal <paschoal@gmail.com>
2025-12-11 23:24:23 -0500

    add vaultwarden to the mix

--- services/k3s/manifests/caddy/config-map.nix
+++ services/k3s/manifests/caddy/config-map.nix
@@ -15,6 +15,14 @@
         }
         reverse_proxy pihole-http.default.svc.cluster.local
       }
+
+      vw.paschoal.me {
+        tls {
+          dns route53 {
+          }
+        }
+        reverse_proxy vaultwarden-http.default.svc.cluster.local
+      }
     '';
   };
 }
