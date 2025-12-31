3a25c86611285d8c4017a1635dd553bb0441fbd9
Matheus Paschoal <paschoal@gmail.com>
2025-12-30 23:33:22 -0500

    longhorn storage class

---
+++ services/k3s/manifests/longhorn/storage-class.nix
@@ -0,0 +1,21 @@
+{
+  apiVersion = "storage.k8s.io/v1";
+  kind = "StorageClass";
+  metadata = {
+    name = "nvme";
+    annotations = {
+      "storageclass.kubernetes.io/is-default-class" = "true";
+    };
+  };
+  provisioner = "driver.longhorn.io";
+  reclaimPolicy = "Delete";
+  volumeBindingMode = "Immediate";
+  allowVolumeExpansion = true;
+  parameters = {
+    backupTargetName = "default";
+    dataEngine = "v1";
+    fsType = "ext4";
+    numberOfReplicas = "2";
+    staleReplicaTimeout = "30";
+  };
+}
