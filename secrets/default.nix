ab7eb207eeecfa863ff55874140f992afc431975
Matheus Paschoal <paschoal@gmail.com>
2026-04-25 21:05:07 -0400

    no longer used keys

--- secrets/default.nix
+++ secrets/default.nix
@@ -12,26 +12,12 @@ in {
   sops.defaultSopsFile = ./secrets.yaml;
 
   sops.age.sshKeyPaths = [
-    "/data/home/.ssh/paschoal@bree"
     "/data/home/.ssh/paschoal@drun"
     "/data/home/.ssh/paschoal@celos"
   ];
   sops.age.generateKey = true;
 
-  sops.secrets."beszel/key" = {};
-  sops.secrets."beszel/drun/token" = {};
-  sops.secrets."beszel/celos/token" = {};
   sops.secrets."k3s/celos/token" = {};
-
-  sops.templates."drun/beszel-agent.env".content = ''
-    KEY=${config.sops.placeholder."beszel/key"}
-    TOKEN=${config.sops.placeholder."beszel/drun/token"}
-  '';
-
-  sops.templates."celos/beszel-agent.env".content = ''
-    KEY=${config.sops.placeholder."beszel/key"}
-    TOKEN=${config.sops.placeholder."beszel/celos/token"}
-  '';
 
   sops.templates."celos/k3s.env".content = ''
     K3S_TOKEN=${config.sops.placeholder."k3s/celos/token"}
