6492850bfe0f5fd21549ad1d66c6c4936c444378
Matheus Paschoal <paschoal@gmail.com>
2026-05-12 10:55:12 -0400

    modularize development block

--- home-manager/development/default.nix
+++ home-manager/development/default.nix
@@ -1,41 +1,147 @@
-{ pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-  home.packages = with pkgs; [
-    postgresql
-    dbeaver-bin
+  options = {
+    development = {
+      sql = {
+        enable = lib.mkOption {
+          default = false;
+          type = lib.types.bool;
+          description = ''
+            Enable SQL tools
+            '';
+        };
+      };
 
-    #
-    # google cloud sdk is broken right now.
-    #
-    (
-      google-cloud-sdk.withExtraComponents [
-        google-cloud-sdk.components.gke-gcloud-auth-plugin
+      coding-agent = {
+        enable = lib.mkOption {
+          default = false;
+          type = lib.types.bool;
+          description = ''
+            Enable agent tools
+          '';
+        };
+      };
+
+      helm = {
+        enable = lib.mkOption {
+          default = false;
+          type = lib.types.bool;
+          description = ''
+            Enable helm and supporting tools
+          '';
+        };
+      };
+
+      dia = {
+        enable = lib.mkOption {
+          default = false;
+          type = lib.types.bool;
+          description = ''
+            Enable DIA
+          '';
+        };
+      };
+
+      sops = {
+        enable = lib.mkOption {
+          default = false;
+          type = lib.types.bool;
+          description = ''
+            Enable SOPS client
+          '';
+        };
+      };
+
+      gcp = {
+        enable = lib.mkOption {
+          default = false;
+          type = lib.types.bool;
+          description = ''
+            Enable Google Cloud Platform CLI
+          '';
+        };
+      };
+
+      k8s = {
+        enable = lib.mkOption {
+          default = false;
+          type = lib.types.bool;
+          description = ''
+            Enable Kubernetes CLI
+          '';
+        };
+      };
+    };
+  };
+
+  config = {
+    home.packages = with pkgs; lib.mkMerge [
+      [
+        hurl
       ]
-    )
 
-    kubernetes
-    sops
-    hurl
-    helmfile
+      (
+        lib.mkIf config.development.gcp.enable [
+          (
+            google-cloud-sdk.withExtraComponents [
+              google-cloud-sdk.components.gke-gcloud-auth-plugin
+            ]
+          )
+        ]
+      )
 
-    (
-      wrapHelm kubernetes-helm {
-        plugins = with pkgs.kubernetes-helmPlugins; [
-          helm-secrets
-          helm-diff
-          helm-git
-        ];
-      }
-    )
+      (
+        lib.mkIf config.development.k8s.enable [
+          kubernetes
+        ]
+      )
 
-    github-copilot-cli
-    opencode
-    dia
-  ];
+      (
+        lib.mkIf config.development.sops.enable [
+          sops
+        ]
+      )
 
-  programs.direnv = {
-    enable = true;
-    nix-direnv.enable = true;
+      (
+        lib.mkIf config.development.dia.enable [
+          dia
+        ]
+      )
+
+      (
+        lib.mkIf config.development.helm.enable [
+          helmfile
+          (
+            wrapHelm kubernetes-helm {
+              plugins = with pkgs.kubernetes-helmPlugins; [
+                helm-secrets
+                helm-diff
+                helm-git
+              ];
+            }
+          )
+        ]
+      )
+
+      (
+        lib.mkIf config.development.sql.enable [
+          postgresql
+          dbeaver-bin
+        ]
+      )
+
+      (
+        lib.mkIf config.development.coding-agent.enable [
+          github-copilot-cli
+          opencode
+        ]
+      )
+    ];
+
+    programs.direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
   };
 }
